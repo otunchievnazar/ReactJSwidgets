0036393dd3d322b69e17920289d694d9ec4a16cb
otunchievnazar <61889210+otunchievnazar@users.noreply.github.com>
2020-12-22 14:31:44 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front-end JavaScript framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'React is a front-end JavaScript framework'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'React is a front-end JavaScript framework'
+    },
+];
+
+export default () => {
+    return (
+    
+        <div>
+            <Search />
+        </div>
+        
+    );
+    
+};
